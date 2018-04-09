bdbb6a14ab388dd3fb886c515433ae1616249993
Shannon Foster <shannon.fos3@outlook.com>
2018-04-09 16:58:35 -0400

    Error with ratings and skills section now. Please fix.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,40 +5,37 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String input;
-		boolean done = false;
+		boolean educationDone = false;
+		boolean experienceDone = false;
+		boolean skillsDone = false;
+		Contact newContact;
 		Experience workHistory;
 		Education schooling;
 		Skills expertise;
-		Skills forGeneralSkills;
-//		int myRatings;
-		String design = "************************************" + '\n'; //for formatting purposes
-//		String[] generalSkills = {"Customer Service", "Computer Operations", "Basics IT Functions", "Microsoft Office"};
 		int i;
-		Ratings myRating = new Ratings();
-		String[] skillRatings = {"n/a", "beginner", "exceptional", "great", "advance", "expert"};
+		Rating myRating = new Rating();
+		String[] skillRatings = {"Still Learning", "Beginner Knowledge", "Exceptional Knowledge", "Intermediate Knowledge", "Advance Knowledge", "Expert Knowledge"};
 
 
 		//Contact Information (store in an array list?)
-		System.out.println('\n' + design + '\n');
+		newContact = new Contact();
 		System.out.println("Use this section to insert your contact information.");
-		Contact newContact = new Contact();
-		System.out.print("What is your first name? ");
+
+		System.out.print("Enter your first name: ");
 		newContact.setFirstName(sc.nextLine());
-		System.out.print("What is your last name? ");
+		System.out.print("Enter your last name: ");
 		newContact.setLastName(sc.nextLine());
-		System.out.print("What is your phone number? ");
+		System.out.print("Enter your phone number: ");
 		newContact.setPhoneNumber(sc.nextLine());
-		System.out.print("What is your email address? ");
+		System.out.print("Enter your email address: ");
 		newContact.setEmailAddress(sc.nextLine());
-//		System.out.print(newContact.contactInfo());
-		System.out.println('\n' + design + '\n');
+
 
 		//Work Experience
-		System.out.println('\n' + design + '\n');
-		System.out.println("Use this section to input your professional work experience.");
+		System.out.println('\n' + "Use this section to input your professional work experience." + '\n');
 		ArrayList<Experience> experience = new ArrayList<>();
 
 
@@ -46,61 +43,56 @@ public class Main {
 		input = sc.nextLine();
 		if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("Yes")) {
 
-			for (int x = 1; x < 5; x++) {
+			do {
 				workHistory = new Experience();
-				System.out.print("Name of the first company: ");
+				System.out.print("Company Name: ");
 				workHistory.setCompany(sc.nextLine());
-				System.out.print("Your job title: ");
+				System.out.print("Job title: ");
 				workHistory.setRole(sc.nextLine());
-				System.out.print("Starting month: ");
+				System.out.print("Start Month: ");
 				workHistory.setStartingMonth(sc.nextLine());
-				System.out.print("Starting year: ");
+				System.out.print("Start Year: ");
 				workHistory.setStartingYear(sc.nextLine());
 				System.out.print("Ending Month: ");
 				workHistory.setEndingMonth(sc.nextLine());
 				System.out.print("Ending year: ");
 				workHistory.setEndingYear(sc.nextLine());
-
 				experience.add(workHistory);
 
 				//Ask user if they want to input any more experience before looping again
 				System.out.print("Do you have any more work experience? (Y/N)");
 				input = sc.nextLine();
-				if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("Yes")) {
-					done = false;
-				} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("No")) {
-					done = true;
+				if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("No")) {
+					experienceDone = true;
 				}
 
-			}
+			} while (!experienceDone);
 
 		}
-		System.out.println('\n' + design + '\n');
 
 		//School/Education History
-		System.out.println('\n' + design + '\n');
-		System.out.print("Use this section to input your educational history." + '\n');
+		System.out.println('\n' + "Use this section to input your educational history." + '\n');
 		ArrayList<Education> education = new ArrayList<>();
 
 		System.out.print("Have you attended any educational institutes? (Y/N) ");
 		input = sc.nextLine();
 		if (input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("Yes")) {
 
-			for (int x = 1; x <= 5; x++) {
+			do {
 				schooling = new Education();
-				System.out.print("What is the name of the institute you attended? ");
+				System.out.print("Institution Name: ");
 				schooling.setSchool(sc.nextLine());
-				System.out.print("What degree did you obtain? ");
+				System.out.print("Degree Obtained: ");
 				schooling.setDegree(sc.nextLine());
-				System.out.print("What was your major? ");
+				System.out.print("Major: ");
 				schooling.setMajor(sc.nextLine());
-				System.out.print("What month did you start? ");
+				System.out.print("Start Month: ");
 				schooling.setStartMonth(sc.nextLine());
-				System.out.print("What year did you start? ");
+				System.out.print("Start Year: ");
 				schooling.setStartYear(sc.nextLine());
-				System.out.print("What was/is your anticipated graduation month? ");
+				System.out.print("Anticipated Graduation Month: ");
 				schooling.setGradMonth(sc.nextLine());
-				System.out.print("What was/is your anticipated graduation year? ");
+				System.out.print("Anticipated Graduation Year: ");
 				schooling.setGradYear(sc.nextLine());
 
 				education.add(schooling);
@@ -108,61 +100,71 @@ public class Main {
 				//Before loop continues, ask user if they have any additional educational experience
 				System.out.print("Do you any additional educational history? (Y/N) ");
 				input = sc.nextLine();
-				if(input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("Yes")) {
-					done = false;
-				} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("No")) {
-					done = true;
+				if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("No")) {
+					educationDone = true;
 				}
-			}
+			} while (!educationDone);
 		}
-		/*System.out.println('\n' + design + '\n');
 
 		//Skills
 		ArrayList<Skills> skills = new ArrayList<>();
+		/*ArrayList<Rating> rating = new ArrayList<>();*/
 
-		System.out.print("Do you have any skills? (Y/N) ");
-		input = sc.nextLine();
-		if(input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("Yes")) {
+		System.out.print("Any skills? (Y/N) ");
+		switch (input = sc.nextLine()) {
+		}
 
-			for(int x=1; x <5; x++) {
+		if ("Y".equalsIgnoreCase(input) || input.equalsIgnoreCase("Yes")) {
+
+			do {
 				expertise = new Skills();
+				myRating = new Rating();
+				i = Integer.parseInt(input);
 				System.out.print("Enter skills: ");
 				expertise.setGeneral(sc.nextLine());
 				System.out.print("How would you rate these skills from 1 to 5? ");
+/*				myRating.setRate(sc.nextLine());
+				myRating.setRate(skillRatings[i]);*/
+
 				input = sc.nextLine();
 				i = Integer.parseInt(input);
 				myRating.setRate(skillRatings[i]);
 				skills.add(expertise);
+				/*rating.add(myRating);//added*/
 
+				//Before loop continues, ask user if they have any additional skills
 				System.out.print("Do you any additional skills? (Y/N) ");
 				input = sc.nextLine();
-				if(input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("Yes")) {
-					done = false;
-				} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("No")) {
-					done = true;
+				if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("No")) {
+					skillsDone = true;
 				}
-			}
-
-
-
-		}*/
+			} while (!skillsDone);
+		}
 
 
 		//The Resume
-		System.out.println('\n'+ design + '\n');
-		System.out.print(newContact.contactInfo());
-		System.out.print("\n");
+		System.out.println('\n' + "*******************************************");
+		System.out.println("Contact");
+		System.out.println("*******************************************");
+		System.out.println(newContact.contactInfo());
+		System.out.println("*******************************************");
+		System.out.println("Experience");
+		System.out.println("*******************************************");
 		for(Experience eachExperience: experience ) {
 			System.out.print(eachExperience.experienceInfo());
 		}
+		System.out.println("*******************************************");
+		System.out.println("Education");
+		System.out.println("*******************************************");
 		for (Education eachEducation: education) {
-			System.out.print(eachEducation.educationInfo());
+			System.out.println(eachEducation.educationInfo());
 		}
-/*		for (Skills eachSkills: skills) {
-			System.out.print(eachSkills.getGeneral()+ "\n" + myRating.thisIsMyRating() + '\t');
-			System.out.print('\n');
-		}*/
-
+		System.out.println("*******************************************");
+		System.out.println("Skills");
+		System.out.println("*******************************************");
+		for (Skills eachSkills: skills) {
+			System.out.println(eachSkills.getGeneral()+ '\n' + "Level: " + myRating.getRate() + '\n');
+		}
 
     }
 }
